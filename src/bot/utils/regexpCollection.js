ef65a4a543b01dafe771b5d0f6f2f6d2bfa72067
Nikolay <fowiflowi@gmail.com>
2019-04-14 19:25:48 +0300

    fix tgId regexp

--- src/bot/utils/regexpCollection.js
+++ src/bot/utils/regexpCollection.js
@@ -1,5 +1,5 @@
 module.exports = {
-  tgId: /^\d{9}$/,
+  tgId: /^\d{5,9}$/,
   mongoId: /^(?=(?:.{12}|.{24})$)[0-9a-fA-F]*$/,
   approveActionTrigger: /^approve\|\d{5,9}\|.+$/,
   rejectActionTrigger: /^reject\|\d{{5,9}\|.+$/,
