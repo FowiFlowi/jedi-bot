e094dea48408116d2fa6b0b914cb8a62c47c8fe5
Nikolay <fowiflowi@gmail.com>
2019-04-17 21:07:31 +0300

    fix /add comamnd

--- src/bot/commands/add.js
+++ src/bot/commands/add.js
@@ -4,8 +4,11 @@ const directionService = require('../service/direction')
 const protect = require('../middlewares/protect')
 
 module.exports = [commands.add, protect.chat(), async ctx => {
-  let [, name] = ctx.message.text.split(' ')
-  name = name.trim()
+  const [, ...nameArray] = ctx.message.text.split(' ')
+  const name = nameArray.join(' ').trim()
+  if (!name) {
+    return ctx.reply('Provide direction name, plaese')
+  }
   try {
     await directionService.insert(name)
   } catch (e) {
