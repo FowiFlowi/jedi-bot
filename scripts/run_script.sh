79a2f511d263d4f2ccb6f14460be5d070acc6136
Mykola Symotiuk <fowiflowi@gmail.com>
2019-10-21 20:43:51 +0300

    fix run_script

--- scripts/run_script.sh
+++ scripts/run_script.sh
@@ -4,9 +4,9 @@ ENV=$1
 SCRIPT_NAME=$2
 [[ $ENV = 'production' ]] && SHORT_ENV="prod" || SHORT_ENV="dev"
 
-if [ $(( SCRIPT_NAME )) -eq "ITKPIautopost" ]; then
+if [ "$SCRIPT_NAME" == "ITKPIautopost" ]; then
     INTERVAL=3
-    if [ $(( ENV )) -eq "production" ]; then
+    if [ "$ENV" == "production" ]; then
         WEEK=$(/bin/date +%v)
     else
         WEEK=$(/bin/date +%V)
@@ -17,7 +17,10 @@ if [ $(( SCRIPT_NAME )) -eq "ITKPIautopost" ]; then
     fi
 fi
 
-GET_CONFIG_COMMAND="node -e "\""const config = require(`${__dirname}/ecosystem.config.js`).apps[0].env_${ENV};\
+CD_COMMAND="cd /home/fowi/node/jedi-bot/${SHORT_ENV}/source"
+$CD_COMMAND
+
+GET_CONFIG_COMMAND="node -e "\""const config = require('./ecosystem.config.js').apps[0].env_${ENV};\
 console.log(config.MONGO_URL, config.BOT_TOKEN, config.TELEGRAPH_ACCESS_TOKEN)"\"""
 CONFIG=$(eval $GET_CONFIG_COMMAND)
 CONFIG_ARRAY=($CONFIG)
@@ -25,8 +28,6 @@ MONGO_URL=${CONFIG_ARRAY[0]}
 BOT_TOKEN=${CONFIG_ARRAY[1]}
 TELEGRAPH_ACCESS_TOKEN=${CONFIG_ARRAY[2]}
 
-CD_COMMAND="cd /home/fowi/node/jedi-bot/${SHORT_ENV}/source"
-$CD_COMMAND
 NODE_COMMAND="REPORT_CHANNEL=${REPORT_CHANNEL} TELEGRAPH_ACCESS_TOKEN=${TELEGRAPH_ACCESS_TOKEN} BOT_TOKEN=${BOT_TOKEN} MONGO_URL=${MONGO_URL} \
 /home/fowi/.nvm/versions/node/v9.8.0/bin/node /home/fowi/node/jedi-bot/${SHORT_ENV}/source/scripts/${SCRIPT_NAME}"
-$NODE_COMMAND
+$(eval $NODE_COMMAND)
