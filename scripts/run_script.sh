53b13dbe95434af65c14f031f64bf86deeb771dc
Mykola Symotiuk <fowiflowi@gmail.com>
2019-11-27 13:19:45 +0200

    change run_script

--- scripts/run_script.sh
+++ scripts/run_script.sh
@@ -17,17 +17,21 @@ if [ "$SCRIPT_NAME" == "ITKPIautopost" ]; then
     fi
 fi
 
-CD_COMMAND="cd /home/fowi/node/jedi-bot/${SHORT_ENV}/source"
+CD_COMMAND="cd /root/repos/jedi-bot/${ENV}"
 $CD_COMMAND
 
 GET_CONFIG_COMMAND="node -e "\""const config = require('./ecosystem.config.js').apps[0].env_${ENV};\
 console.log(config.MONGO_URL, config.BOT_TOKEN, config.TELEGRAPH_ACCESS_TOKEN)"\"""
 CONFIG=$(eval $GET_CONFIG_COMMAND)
 CONFIG_ARRAY=($CONFIG)
+
 MONGO_URL=${CONFIG_ARRAY[0]}
 BOT_TOKEN=${CONFIG_ARRAY[1]}
 TELEGRAPH_ACCESS_TOKEN=${CONFIG_ARRAY[2]}
 
-NODE_COMMAND="NODE_ENV=${ENV} REPORT_CHANNEL=${REPORT_CHANNEL} TELEGRAPH_ACCESS_TOKEN=${TELEGRAPH_ACCESS_TOKEN} BOT_TOKEN=${BOT_TOKEN} MONGO_URL=${MONGO_URL} \
-/home/fowi/.nvm/versions/node/v9.8.0/bin/node /home/fowi/node/jedi-bot/${SHORT_ENV}/source/scripts/${SCRIPT_NAME}"
+SOURCE_COMMAND="source /root/.nvm/nvm.sh"
+$SOURCE_COMMAND
+
+NODE_COMMAND="NODE_ENV=${ENV} REPORT_CHANNEL=${REPORT_CHANNEL} TELEGRAPH_ACCESS_TOKEN=${TELEGRAPH_ACCESS_TOKEN} \
+BOT_TOKEN=${BOT_TOKEN} MONGO_URL=${MONGO_URL} node scripts/${SCRIPT_NAME}"
 eval $NODE_COMMAND
